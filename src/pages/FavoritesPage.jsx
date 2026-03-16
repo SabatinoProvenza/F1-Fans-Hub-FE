e9c4f9945dd55d0da1947852bd3eef9f89521de3
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-03-16 14:00:13 +0100

    adds selects results in FavoritePages

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { Link } from "react-router-dom"
 import Modal from "react-bootstrap/Modal"
 import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
 import LoadingSpinner from "../components/Spinner/LoadingSpinner"
 
 const FavoritesPage = () => {
@@ -11,6 +12,8 @@ const FavoritesPage = () => {
 
   const [showModal, setShowModal] = useState(false)
   const [selectedArticle, setSelectedArticle] = useState(null)
+
+  const [sortBy, setSortBy] = useState("saved-desc")
 
   const openRemoveModal = (article) => {
     setSelectedArticle(article)
@@ -42,7 +45,6 @@ const FavoritesPage = () => {
         if (!res.ok) throw new Error("Errore nel caricamento preferiti")
 
         const data = await res.json()
-
         setFavorites(data)
       } catch (e) {
         setError(e.message)
@@ -55,10 +57,12 @@ const FavoritesPage = () => {
   }, [])
 
   const handleRemoveFavorite = async () => {
+    if (!selectedArticle) return
+
     const articleGuid = selectedArticle.guid
     const token = localStorage.getItem("token")
 
-    if (!token || !selectedArticle) return
+    if (!token) return
 
     try {
       const res = await fetch(
@@ -75,15 +79,44 @@ const FavoritesPage = () => {
         throw new Error("Errore nella rimozione dai preferiti")
       }
 
-      // aggiorna la lista locale
       setFavorites((prev) =>
         prev.filter((article) => article.guid !== articleGuid),
       )
+
       closeRemoveModal()
     } catch (error) {
       console.error(error)
     }
   }
+
+  const sortedFavorites = useMemo(() => {
+    const favoritesCopy = [...favorites]
+
+    return favoritesCopy.sort((a, b) => {
+      const pubDateA = new Date(a.pubDate)
+      const pubDateB = new Date(b.pubDate)
+
+      const savedAtA = new Date(a.savedAt)
+      const savedAtB = new Date(b.savedAt)
+
+      switch (sortBy) {
+        case "pubDate-desc":
+          return pubDateB - pubDateA
+
+        case "pubDate-asc":
+          return pubDateA - pubDateB
+
+        case "savedAt-desc":
+          return savedAtB - savedAtA
+
+        case "savedAt-asc":
+          return savedAtA - savedAtB
+
+        default:
+          return 0
+      }
+    })
+  }, [favorites, sortBy])
 
   if (loading) return <LoadingSpinner />
 
@@ -91,15 +124,36 @@ const FavoritesPage = () => {
 
   return (
     <>
-      <div className=" container py-5 text-white page-enter ">
-        <h1 className="my-5 text-center">I tuoi preferiti</h1>
+      <div className="container py-5 text-white page-enter">
+        <div className="my-5 d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-3">
+          <h1 className="m-0 text-center text-md-start">I tuoi preferiti</h1>
+
+          {favorites.length > 0 && (
+            <div>
+              <Form.Select
+                value={sortBy}
+                onChange={(e) => setSortBy(e.target.value)}
+                className="outline-primary "
+              >
+                <option value="savedAt-desc">Salvati più recentemente</option>
+                <option value="savedAt-asc">Salvati meno recentemente</option>
+                <option value="pubDate-desc">
+                  Pubblicati più recentemente
+                </option>
+                <option value="pubDate-asc">
+                  Pubblicati meno recentemente
+                </option>
+              </Form.Select>
+            </div>
+          )}
+        </div>
 
         {favorites.length === 0 && (
           <p className="text-primary">Nessun articolo salvato.</p>
         )}
 
         <div className="row g-4">
-          {favorites.map((article) => (
+          {sortedFavorites.map((article) => (
             <div className="col-md-6 col-lg-4" key={article.articleId}>
               <div className="card article-card h-100 text-white border-0 d-flex flex-column">
                 <img
@@ -109,10 +163,22 @@ const FavoritesPage = () => {
                   style={{ height: "200px", objectFit: "cover" }}
                 />
 
-                <div className="card-body d-flex flex-column ">
+                <div className="card-body d-flex flex-column">
                   <h5 className="card-title">{article.title}</h5>
 
-                  <p className="card-text text-muted small">{article.source}</p>
+                  <p className="card-text text-muted small mb-1">
+                    {article.source}
+                  </p>
+
+                  <p className="card-text text-muted small mb-1">
+                    Pubblicato:{" "}
+                    {new Date(article.pubDate).toLocaleDateString("it-IT")}
+                  </p>
+
+                  <p className="card-text text-muted small mb-3">
+                    Salvato:{" "}
+                    {new Date(article.savedAt).toLocaleDateString("it-IT")}
+                  </p>
 
                   <div className="d-flex justify-content-between mt-auto">
                     <Link
@@ -121,6 +187,7 @@ const FavoritesPage = () => {
                     >
                       Leggi articolo
                     </Link>
+
                     <button
                       className="btn btn-primary"
                       onClick={() => openRemoveModal(article)}
@@ -134,6 +201,7 @@ const FavoritesPage = () => {
           ))}
         </div>
       </div>
+
       <Modal
         show={showModal}
         onHide={closeRemoveModal}
